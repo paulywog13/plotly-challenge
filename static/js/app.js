e0c8d7078f7fa10e9e012757482abf6c7d210fda
Pauly Richmeier <paul_richmeier@yahoo.com>
2020-12-23 14:01:30 -0700

    Code added for sorting and graphing data.

--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,73 @@
-// Read in samples.json data for file
-d3.json(samples.json, function(data) {
+// Writing main fuction
+function dashboard() {
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset");
+
+// Use the D3 library to read in `samples.json`.
+d3.json("/data/samples.json").then((importedData) => {
+    var data = importedData;
     console.log(data);
-});    
+    data.names.forEach(d =>{
+        dropdownMenu.append("option").text(d).property("value");
+    });
+    optionChanged(data.names[0])
 
-    
-// // Create an array of each sample's numbers
-// var 940 = Object.values(names.940);
-// // var uk = Object.values(data.uk);
-// // var canada = Object.values(data.canada);
+});   
+};
+dashboard() 
 
-// // Create an array of music provider labels
-// var labels = Object.keys(names.940);
 
-// // Display the default plot
-// function init() {
-//     var data = [{
-//       values: 940,
-//       labels: labels,
-//       type: "pie"
-//     }];
-  
-//     var layout = {
-//       height: 600,
-//       width: 800
-//     };
-  
-//     Plotly.newPlot("pie", data, layout);
-//   }
+function optionChanged(subjectId) {
+    d3.json("/data/samples.json").then((data) => {
+    var demoInfo = data.metadata.filter(md => md.id == subjectId)
+    var firstDemoInfo = demoInfo[0]
+    console.log(firstDemoInfo)
+    console.log(demoInfo)
+    var demographInfo = d3.select("#sample-metadata");
+    // Needing to clear the list for the change
+    demographInfo.html("")
+    // Appending the demographic info box 
+    Object.entries(firstDemoInfo).forEach(([key, value]) => {
+        demographInfo.append("p").text(`${key}:${value}`)
+    })
+    // Inputting data for bar chart
+    var graphInfo = data.samples.filter(sd => sd.id == subjectId)
+    var firstGraphInfo = graphInfo[0]
+    // Slice for the top ten otu values
+    var otuIds = firstGraphInfo.otu_ids.slice(0, 10).reverse().map(otus => `otu Id:${otus}` )
+    var sampVals = firstGraphInfo.sample_values.slice(0, 10).reverse()
+    var otuLabels = firstGraphInfo.otu_labels.slice(0, 10).reverse() 
+    // Creating trace for bar chart setup
+    trace1 = [{
+        x: sampVals, 
+        y: otuIds, 
+        text: otuLabels,
+        type: 'bar',
+        orientation: "h"
+    }]
+    layout = {
+        title: "Belly Button Bar Chart"
+    }
+    Plotly.newPlot("bar", trace1, layout)
+    // Trace for the bubble charts
+    trace2 = [{
+        x: firstGraphInfo.otu_ids, 
+        y: firstGraphInfo.sample_values,
+        text: firstGraphInfo.otu_label,
+        mode: "markers",
+        marker: {color: firstGraphInfo.otu_ids, size:  firstGraphInfo.sample_values}
+    }]
+    Plotly.newPlot("bubble", trace2)
+    })};
+
+
+// // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+// // Sort the otu data by samples
+// data.sort(
+//     function(a, b) {
+//         if (a.names === b.) 
+//     }
+//     )
 
 // // Call updatePlotly() when a change takes place to the DOM
 // d3.selectAll("#selDataset").on("change", getData);
@@ -46,10 +87,10 @@ d3.json(samples.json, function(data) {
 //     updatePlotly(data);
 // }
 
-// // Update the restyled plot's values
-// function updatePlotly(newdata) {
-//   Plotly.restyle("pie", "values", [newdata]);
-// }
+// // // Update the restyled plot's values
+// // function updatePlotly(newdata) {
+// //   Plotly.restyle("pie", "values", [newdata]);
+// // }
   
-// init();
+// // init();
   
